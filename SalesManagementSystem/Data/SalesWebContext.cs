9869d62c6a9b3be7775b328cab45c7d75485cb20
caiocezzar <caiocezzarpng@gmail.com>
2023-11-05 14:36:26 -0300

    feat: Criação da estrutura principal do projeto em NET 6

---
+++ SalesManagementSystem/Data/SalesWebContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesManagementSystem.Models;
+
+namespace SalesManagementSystem.Data
+{
+    public class SalesWebContext : DbContext
+    {
+        public SalesWebContext(DbContextOptions<SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
--- SalesManagementSystem/Models/Seller.cs
+++ SalesManagementSystem/Models/Seller.cs
@@ -15,10 +15,10 @@ namespace SalesManagementSystem.Models
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
@@ -26,19 +26,16 @@ namespace SalesManagementSystem.Models
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
-
-        public Department Department { get; set; }
+        public Department? Department { get; set; }
         public int DepartmentId { get; set; }
-
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        public Seller(string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
-            Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
