46adcb1aaabf50af8aa06715d62e8658bcbe60f6
caiocezzar <caiocezzarpng@gmail.com>
2023-11-21 21:41:46 -0300

    refactor: ajustes na controller, model e views

--- SalesManagementSystem/Models/Department.cs
+++ SalesManagementSystem/Models/Department.cs
@@ -1,13 +1,19 @@
-﻿namespace SalesManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesManagementSystem.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
+            this.Name = string.Empty;
         }
 
         public Department(string name)
